77472fa0879aec723fbd79569230de44c74de6e1
Lisandro <lisandro.vera8579@gmail.com>
2024-01-07 13:43:09 -0500

    DiningReview

--- DiningReview/src/main/java/com/LisandroVera/DiningReview/DiningReviewApplication.java
+++ DiningReview/src/main/java/com/LisandroVera/DiningReview/DiningReviewApplication.java
@@ -1,5 +1,5 @@
 package com.LisandroVera.DiningReview;
-// use http://localhost:8080/h2-console for H2 console
+// use http://localhost:8080/h2-console for H2 console, use embedded
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
