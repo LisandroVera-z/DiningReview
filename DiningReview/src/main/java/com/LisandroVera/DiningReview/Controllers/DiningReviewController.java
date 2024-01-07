7885606072010ada4deed3de6c0c58e7ce47f770
Lisandro <lisandro.vera8579@gmail.com>
2024-01-07 00:40:51 -0500

    DiningReview

--- DiningReview/src/main/java/com/LisandroVera/DiningReview/Controllers/DiningReviewController.java
+++ DiningReview/src/main/java/com/LisandroVera/DiningReview/Controllers/DiningReviewController.java
@@ -1,4 +1,20 @@
 package com.LisandroVera.DiningReview.Controllers;
 
+import com.LisandroVera.DiningReview.Repositories.DiningReviewRepository;
+import com.LisandroVera.DiningReview.Repositories.RestaurantRepository;
+import com.LisandroVera.DiningReview.Repositories.UserRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/DiningReviews")
 public class DiningReviewController {
+    private final DiningReviewRepository diningReviewRepository;
+    private final UserRepository userRepository;
+    private final RestaurantRepository restaurantRepository;
+    public DiningReviewController(DiningReviewRepository diningReviewRepository, UserRepository userRepository, RestaurantRepository restaurantRepository) {
+        this.diningReviewRepository = diningReviewRepository;
+        this.userRepository = userRepository;
+        this.restaurantRepository = restaurantRepository;
+    }
 }
