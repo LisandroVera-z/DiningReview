5319df82dfdaf948e1d4bb7a055114d208f46ee9
Lisandro <lisandro.vera8579@gmail.com>
2024-01-06 21:08:10 -0500

    DiningReview

---
+++ DiningReview/src/main/java/com/LisandroVera/DiningReview/DiningReviewRepository.java
@@ -0,0 +1,4 @@
+package com.LisandroVera.DiningReview;
+
+public interface DiningReviewRepository {
+}
---
+++ DiningReview/src/main/java/com/LisandroVera/DiningReview/Restaurant.java
@@ -0,0 +1,48 @@
+package com.LisandroVera.DiningReview;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@Entity
+@Table(name = "restaurants")
+public class Restaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String address;
+
+    private Double peanutScore;
+    private Double eggScore;
+    private Double dairyScore;
+    private Double overallScore;
+
+    public Double calculateOverallScore() {
+        int count = 0;
+        double totalScore = 0.0;
+
+        if (peanutScore != null) {
+            totalScore += peanutScore;
+            count++;
+        }
+        if (eggScore != null) {
+            totalScore += eggScore;
+            count++;
+        }
+        if (dairyScore != null) {
+            totalScore += dairyScore;
+            count++;
+        }
+
+        // Avoid division by zero
+        return count > 0 ? totalScore / count : null;
+    }
+}
+
+
