2f3595966c4c955dbf7e00614fc68cfe779df31b
Lisandro <lisandro.vera8579@gmail.com>
2024-01-07 14:55:04 -0500

    DiningReview

--- DiningReview/src/main/java/com/LisandroVera/DiningReview/Controllers/RestaurantController.java
+++ DiningReview/src/main/java/com/LisandroVera/DiningReview/Controllers/RestaurantController.java
@@ -1,15 +1,55 @@
 package com.LisandroVera.DiningReview.Controllers;
 
+import com.LisandroVera.DiningReview.Entities.Restaurant;
 import com.LisandroVera.DiningReview.Repositories.RestaurantRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/Restaurants")
+@RequestMapping("/restaurants")
 public class RestaurantController {
     private final RestaurantRepository restaurantRepository;
+
     private RestaurantController(RestaurantRepository restaurantRepository) {
         this.restaurantRepository = restaurantRepository;
     }
 
+    //Find restaurant by its unique ID
+    @GetMapping
+    public Restaurant getById(@RequestParam Integer id) {
+        Optional<Restaurant> restaurantOptional = restaurantRepository.findById(id);
+        if (!restaurantOptional.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return restaurantOptional.get();
+    }
+
+    //Find restaurants by zip code and sorted by their score for a specific allergy, descending order
+    @GetMapping("/{zipCode}")
+    public List<Restaurant> getByZipCode(@PathVariable Integer zipCode, @RequestParam String allergy) {
+        if (allergy.toLowerCase() == "peanut") {
+            return restaurantRepository.findByZipCodeOrderByPeanutScoreDesc(zipCode);
+        } else if (allergy.toLowerCase() == "egg") {
+            return restaurantRepository.findByZipCodeOrderByEggScoreDesc(zipCode);
+        } else if (allergy.toLowerCase() == "dairy") {
+            return restaurantRepository.findByZipCodeOrderByDairyScoreDesc(zipCode);
+        } else {
+            return restaurantRepository.findByZipCode(zipCode);
+        }
+    }
+
+    //Add restaurant
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public Restaurant addRestaurant(@RequestBody Restaurant restaurantToAdd) {
+        if (restaurantRepository.existsByName(restaurantToAdd.getName()) && restaurantRepository.existsByZipCode(restaurantToAdd.getZipCode())) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        restaurantRepository.save(restaurantToAdd);
+        return restaurantToAdd;
+    }
 }
--- DiningReview/src/main/java/com/LisandroVera/DiningReview/Repositories/RestaurantRepository.java
+++ DiningReview/src/main/java/com/LisandroVera/DiningReview/Repositories/RestaurantRepository.java
@@ -3,8 +3,17 @@ package com.LisandroVera.DiningReview.Repositories;
 import com.LisandroVera.DiningReview.Entities.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Integer> {
+    List<Restaurant> findByZipCodeOrderByPeanutScoreDesc(Integer zipCode);
+    List<Restaurant> findByZipCodeOrderByEggScoreDesc(Integer zipCode);
+    List<Restaurant> findByZipCodeOrderByDairyScoreDesc(Integer zipCode);
+    List<Restaurant> findByZipCode(Integer zipCode);
+    Boolean existsByName(String name);
+    Boolean existsByZipCode(Integer zipCode);
 
 
 }
